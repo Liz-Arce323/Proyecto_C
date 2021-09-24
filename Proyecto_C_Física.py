330ada42460094eb9f49f7273ca7deb3cf70367d
Liz-Arce323 <88904346+Liz-Arce323@users.noreply.github.com>
2021-09-24 18:52:36 -0500

    Remplazando archivo

--- Proyecto_C_Física.py
+++ Proyecto_C_Física.py
@@ -1,9 +1,9 @@
-######## Constantes ########
+"""Constantes"""
 
-Gravedad = 9.81
+GRAVEDAD = 9.81
 
 
-########### Calificación ##############
+""" Calcula la calificación del usuario"""
 
 def calificacion (respuesta_t1n1_1, respuesta_t1n1_2, respuesta_t1n1_3):
 
@@ -38,21 +38,20 @@ def calificacion (respuesta_t1n1_1, respuesta_t1n1_2, respuesta_t1n1_3):
 
     calif = (cont*100/3)
 
-    print ("Tu calificación final es:", calif)
+    print ("Tu calificación final es:", ("{:.2f}".format(calif)))
     
 
 """ Se hará uso de condicionales donde se compraran las respuestas del usuario
 con los resultados obtenidos en python, determinando si el resultado es
-correcto o incorrecto (le mostrará esto al usuario, y se le sumará ciertos puntos
-a la calificación final"""
+correcto o incorrecto (le mostrará esto al usuario, y se le sumará ciertos
+puntos a la calificación final"""
 
 #Solo un ejemplo de como serán usadas las condicionales, agregar el uso de listas para guardar las respuestas dadas por el usuario
 #Modificar cuando se encuentre una manera de hacerlo más corto y eficiente 
 
 
 
-########### Tema 1_Operaciones ###################
-# Notación científica
+""" Tema 1_Operaciones_Notación científica"""
 
 def notacion_cientifica(numero_nd):
     return format(numero_nd, ".1E")
@@ -61,8 +60,7 @@ def notacion_decimal(numero_nc):
     return float(numero_nc)
 
 
-########### Tema 1_Preguntas_#####################
-# Notación científica
+"""Tema 1_Preguntas_Notación científica"""
 
 def tema1_dificultad1(tema_e, dificultad_e):
 
@@ -112,16 +110,16 @@ Para el número en notación científica a notación decimal:
 
 def tema1_dificultad2(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil")
+    print ("La misma temática de nivel uno, pero más difícil")
     
     
 def tema1_dificultad3(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil que uno y dos")
+    print ("La misma temática de nivel uno, pero más difícil que uno y dos")
 
 
 
-########### Tema 2_Operaciones ###################
+"""" Tema 2_Operaciones"""
 
 #MRU
 
@@ -188,8 +186,8 @@ def aceleracion_mrua_v0vfd (velocidad_f_mrua, velocidad_i_mrua, distancia_mrua):
 
 
 
-########### Tema 2_Preguntas #####################
-#MRU y MRUA
+""" Tema 2_Preguntas_MRU y MRUA"""
+
 
 def tema2_dificultad1(tema_e, dificultad_e):
 
@@ -236,23 +234,25 @@ pasó a tener una velocidad de 45 m/s en 89 segundos?
 
 def tema2_dificultad2(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma tematica de nivel uno pero más difícil")
+    print ("La misma tematica de nivel uno, pero más difícil")
     
 def tema2_dificultad3(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma tematica de nivel uno pero más difícil que uno y dos")
+    print ("La misma tematica de nivel uno, pero más difícil que uno y dos")
 
 
-########### Tema 3_Operaciones ###################
-#Caída Libre 
+    
+
+
+"""Tema 3_Operaciones_Caída Libre"""
 
 """ Altura """
 
 def altura_cl_t (tiempo_cl):
-    return ("{:.2f}".format((1/2)*Gravedad*tiempo_cl**2))
+    return ("{:.2f}".format((1/2)*GRAVEDAD*tiempo_cl**2))
 
 def altura_cl_vit (velocidad_i_cl, tiempo_cl):
-    return ("{:.2f}".format((velocidad_i_cl*tiempo_cl)+(1/2)*Gravedad* tiempo_cl**2))
+    return ("{:.2f}".format((velocidad_i_cl*tiempo_cl)+(1/2)*GRAVEDAD* tiempo_cl**2))
 
 def altura_cl_vivft (velocidad_i_cl, velocidad_f_cl, tiempo_cl):
     return ("{:.2f}".format(((velocidad_i_cl + velocidad_f_cl)/2)*tiempo_cl))
@@ -261,10 +261,10 @@ def altura_cl_vivft (velocidad_i_cl, velocidad_f_cl, tiempo_cl):
 """ Tiempo """
 
 def tiempo_cl_a (altura_cl):
-    return ("{:.2f}".format(((2*altura_cl)/Gravedad)**(1/2)))
+    return ("{:.2f}".format(((2*altura_cl)/GRAVEDAD)**(1/2)))
 
 def tiempo_cl_vivf (velocidad_i_cl, velocidad_f_cl):
-    return ("{:.2f}".format((velocidad_f_cl-velocidad_i_cl)/Gravedad))
+    return ("{:.2f}".format((velocidad_f_cl-velocidad_i_cl)/GRAVEDAD))
 
 def tiempo_cl_vivfa (velocidad_i_cl, velocidad_f_cl, altura_cl):
     return ("{:.2f}".format(altura_cl/((velocidad_i_cl+velocidad_f_cl)/2)))
@@ -273,13 +273,13 @@ def tiempo_cl_vivfa (velocidad_i_cl, velocidad_f_cl, altura_cl):
 """ Velocidad Inicial """
 
 def velocidad_i_cl_vft (velocidad_f_cl, tiempo_cl):
-    return ("{:.2f}".format(velocidad_f_cl-Gravedad*tiempo_cl))
+    return ("{:.2f}".format(velocidad_f_cl-GRAVEDAD*tiempo_cl))
 
 def velocidad_i_cl_vfa (velocidad_f_cl, altura_cl):
-    return ("{:.2f}".format((velocidad_f_cl**2-2*Gravedad*altura_cl)**(1/2)))
+    return ("{:.2f}".format((velocidad_f_cl**2-2*GRAVEDAD*altura_cl)**(1/2)))
 
 def velocidad_i_cl_at (altura_cl, tiempo_cl):
-    return ("{:.2f}".format((altura_cl-(1/2)*Gravedad*(tiempo_cl**2))/tiempo_cl))
+    return ("{:.2f}".format((altura_cl-(1/2)*GRAVEDAD*(tiempo_cl**2))/tiempo_cl))
 
 def velocidad_i_cl_vfat (velocidad_f_cl, altura_cl, tiempo_cl):
     return ("{:.2f}".format(((2*altura_cl)/tiempo_cl)-velocidad_f_cl))
@@ -288,17 +288,16 @@ def velocidad_i_cl_vfat (velocidad_f_cl, altura_cl, tiempo_cl):
 """ Velocidad Final """
 
 def velocidad_f_cl_vit (velocidad_i_cl, tiempo_cl):
-    return ("{:.2f}".format(velocidad_i_cl+Gravedad*tiempo_cl))
+    return ("{:.2f}".format(velocidad_i_cl+GRAVEDAD*tiempo_cl))
 
 def velocidad_f_cl_via (velocidad_i_cl, altura_cl):
-    return ("{:.2f}".format((velocidad_i_cl**2+2*Gravedad*altura_cl)**(1/2)))
+    return ("{:.2f}".format((velocidad_i_cl**2+2*GRAVEDAD*altura_cl)**(1/2)))
 
 def velocidad_f_cl_viat (velocidad_i_cl, altura_cl, tiempo_cl):
     return ("{:.2f}".format(((2*altura_cl)/tiempo_cl)-velocidad_i_cl))
 
 
-########### Tema 3_Preguntas #####################
-#Caída Libre 
+"""Tema 3_Preguntas_Caída Libre"""
 
 def tema3_dificultad1(tema_e, dificultad_e):
 
@@ -348,26 +347,27 @@ y que tiene una velocidad inicial de 13 m/s?
 
 def tema3_dificultad2(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil")
+    print ("La misma temática de nivel uno, pero más difícil")
     
 def tema3_dificultad3(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil que uno y dos")
+    print ("La misma temática de nivel uno, pero más difícil que uno y dos")
     
 
 
-########### Tema 4_Operaciones ###################
-#Masa y Peso
+
+
+
+"""Tema 4_Operaciones_Masa y Peso"""
 
 def masa_mp (peso):
-    return ("{:.2f}".format(peso/Gravedad))
+    return ("{:.2f}".format(peso/GRAVEDAD))
 
 def peso_mp (masa):
-    return ("{:.2f}".format(masa*Gravedad))
+    return ("{:.2f}".format(masa*GRAVEDAD))
 
    
-########### Tema 4_Preguntas #####################
-#Masa y Peso
+"""Tema 4_Preguntas_Masa y Peso"""
 
 def tema4_dificultad1(tema_e, dificultad_e):
 
@@ -412,15 +412,17 @@ Calcula la masa de un automovil que pesa 17500 N
 
 def tema4_dificultad2(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil")
+    print ("La misma temática de nivel uno, pero más difícil")
     
 def tema4_dificultad3(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil que uno y dos")
+    print ("La misma temática de nivel uno, pero más difícil que uno y dos")
+
+    
     
 
-########### Tema 5_Operaciones ###################
-#Leyes de Newton
+"""Tema 5_Operaciones_Leyes de Newton"""
+
 def fuerza_neta (masa_n, aceleracion_n):
     return ("{:.2f}".format(masa_n *aceleracion_n))
 
@@ -429,9 +431,9 @@ def masa_2n (fuerza_n, aceleracion_n):
 
 def aceleracion_2n (fuerza_n, masa_n):
     return ("{:.2f}".format(fuerza_n/masa_n))
+
     
-########### Tema 5_Preguntas #####################
-#Leyes de Newton
+"""Tema 5_Preguntas_Leyes de Newton"""
 
 def tema5_dificultad1(tema_e, dificultad_e):
     
@@ -480,127 +482,148 @@ c) Ley de los objetos estáticos
 
 def tema5_dificultad2(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil")
+    print ("La misma temática de nivel uno, pero más difícil")
     
 def tema5_dificultad3(tema_e, dificultad_e):
     print ("Tema 1: " + tema_e + "\n\nNivel " + dificultad_e)
-    print ("La misma temática de nivel uno pero más difícil que uno y dos")
+    print ("La misma temática de nivel uno, pero más difícil que uno y dos")
+
+
+    
     
 
+"""Elegir  tema y nivel"""
 
-
-
-################## Algoritmo principal ###############################
-
-print ("Hola usuario! ¿Qué tema deseas repasar? Por favor ingresa el número")
-
-tema = int(input("""
-
-1.- Notación científica
-2.- MRU y MRUA
-3.- Caída Libre 
-4.- Masa y Peso
-5.- Leyes de Newton
-
-"""))
-
-
-if tema < 1 or tema > 5:    #Esta función ya estaba presente en las entregas pasadas, pero es utilizada para cumplir el avance de if's para esta entrega
+def tema_y_nivel ():
 
     tema = int(input("""
-Lo siento el número seleccionado no está en los parámetros,
-por favor ingrese un número que este dentro de los parámetros:
 
-1.- Notación científica
-2.- MRU y MRUA
-3.- Caída Libre 
-4.- Masa y Peso
-5.- Leyes de Newton
+    1.- Notación científica
+    2.- MRU y MRUA
+    3.- Caída Libre 
+    4.- Masa y Peso
+    5.- Leyes de Newton
 
-"""))
+    """))
 
 
-dificultad = int(input("""
-¿Qué nivel deseas completar? Escribe el número:
+    if tema < 1 or tema > 5:    
 
-1.- Sencillo 
-2.- Normal
-3.- Avanzado
+        tema = int(input("""
+    Lo siento el número seleccionado no está en los parámetros,
+    por favor ingrese un número que este dentro de los parámetros:
 
-"""))
+    1.- Notación científica
+    2.- MRU y MRUA
+    3.- Caída Libre 
+    4.- Masa y Peso
+    5.- Leyes de Newton
 
-if dificultad < 0 or dificultad > 3:    #Esta función ya estaba presente en las entregas pasadas, pero es utilizada para cumplir el avance de if's para esta entrega
+    """))
+
 
     dificultad = int(input("""
-Lo siento el número seleccionado no está en los parámetros,
-por favor ingrese un número que este dentro de los parámetros:
+    ¿Qué nivel deseas completar? Escribe el número:
 
-1.- Sencillo 
-2.- Normal
-3.- Avanzado
-"""))
+    1.- Sencillo 
+    2.- Normal
+    3.- Avanzado
+
+    """))
+
+    if dificultad < 0 or dificultad > 3:    
+
+        dificultad = int(input("""
+    Lo siento el número seleccionado no está en los parámetros,
+    por favor ingrese un número que este dentro de los parámetros:
+
+    1.- Sencillo 
+    2.- Normal
+    3.- Avanzado
+    """))
+
+
+        
+    if tema == 1 and dificultad == 1:
+        tema1_dificultad1("Notación Cinetífica", "1")
+
+    elif tema == 1 and dificultad == 2:
+        tema1_dificultad2("Notación Cinetífica", "2")
+
+    elif tema == 1 and dificultad == 3:
+        tema1_dificultad3 ("Notación Cinetífica", "3")
 
 
 
-#Estas funciones ya estaban presentes en las entregas pasadas, pero es utilizada para cumplir el avance de if's para esta entrega
+    elif tema == 2 and dificultad == 1:
+        tema2_dificultad1("MRU y MRUA", "1")
+
+    elif tema == 2 and dificultad == 2:
+        tema2_dificultad2("MRU y MRUA", "2")
+
+
+    elif tema == 2 and dificultad == 3:
+        tema2_dificultad3("MRU y MRUA", "3")
+
+
+
+    elif tema == 3 and dificultad == 1:
+        tema3_dificultad1("Caída Libre", "1")
+
+    elif tema == 3 and dificultad == 2:
+        tema3_dificultad2("Caída Libre", "2")
+
+
+    elif tema == 3 and dificultad == 3:
+        tema3_dificultad3("Caída Libre", "3")
+
+
+
+    elif tema == 4 and dificultad == 1:
+        tema4_dificultad1("Masa y Peso", "1")
+
+    elif tema == 4 and dificultad == 2:
+        tema4_dificultad2("Masa y Peso", "2")
+
+
+    elif tema == 4 and dificultad == 3:
+        tema4_dificultad3("Masa y Peso", "3")
+
+
+
+    elif tema == 5 and dificultad == 1:
+        tema5_dificultad1("Leyes de Newton", "1")
+
+    elif tema == 5 and dificultad == 2:
+        tema5_dificultad2("Leyes de Newton", "2")
+
+
+    elif tema == 5 and dificultad == 3:
+        tema5_dificultad3("Leyes de Newton", "3")
+
+
+
+
+
+
+
+""" Algoritmo principal"""
+
+        
+
+print ("¡Hola usuario! ¿Qué tema deseas repasar? Por favor ingresa el número")
+
+tema_y_nivel ()
+
+
+continuar_p = input("¿Deseas repasar otro tema?\n a)Sí\n b)No\n")
+
+while continuar_p == "a" or continuar_p == "A":
     
-if tema == 1 and dificultad == 1:
-    tema1_dificultad1("Notación Cinetífica", "1")
-
-elif tema == 1 and dificultad == 2:
-    tema1_dificultad2("Notación Cinetífica", "2")
-
-elif tema == 1 and dificultad == 3:
-    tema1_dificultad3 ("Notación Cinetífica", "3")
+    tema_y_nivel ()
+    
+    continuar_p = input("¿Deseas repasar otro tema?\n a)Sí\n b)No\n")
 
 
-
-elif tema == 2 and dificultad == 1:
-    tema2_dificultad1("MRU y MRUA", "1")
-
-elif tema == 2 and dificultad == 2:
-    tema2_dificultad2("MRU y MRUA", "2")
-
-
-elif tema == 2 and dificultad == 3:
-    tema2_dificultad3("MRU y MRUA", "3")
-
-
-
-elif tema == 3 and dificultad == 1:
-    tema3_dificultad1("Caída Libre", "1")
-
-elif tema == 3 and dificultad == 2:
-    tema3_dificultad2("Caída Libre", "2")
-
-
-elif tema == 3 and dificultad == 3:
-    tema3_dificultad3("Caída Libre", "3")
-
-
-
-elif tema == 4 and dificultad == 1:
-    tema4_dificultad1("Masa y Peso", "1")
-
-elif tema == 4 and dificultad == 2:
-    tema4_dificultad2("Masa y Peso", "2")
-
-
-elif tema == 4 and dificultad == 3:
-    tema4_dificultad3("Masa y Peso", "3")
-
-
-
-elif tema == 5 and dificultad == 1:
-    tema5_dificultad1("Leyes de Newton", "1")
-
-elif tema == 5 and dificultad == 2:
-    tema5_dificultad2("Leyes de Newton", "2")
-
-
-elif tema == 5 and dificultad == 3:
-    tema5_dificultad3("Leyes de Newton", "3")
-
-
-# print (calificacion ())
+print ("¡Gracias por repasar en este programa!")
 
